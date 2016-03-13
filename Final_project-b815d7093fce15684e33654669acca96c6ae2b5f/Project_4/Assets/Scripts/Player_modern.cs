311c154d6a03d43cdf4d9fdc872bba58acf8c74c
YizhouLuo <986732031@qq.com>
2016-03-13 16:16:08 -0400

    player attack

---
+++ Final_project-b815d7093fce15684e33654669acca96c6ae2b5f/Project_4/Assets/Scripts/Player_modern.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_modern : MonoBehaviour {
+	//animation for attack
+	private Animation				Anim;
+	public AnimationClip 			Melee_Clip;
+
+	//melee attack cool down
+	private float 					meleeRate = 0.3f;
+	private float 					nextMelee = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		Anim = this.GetComponent<Animation> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A) && Time.time > nextMelee) {
+			Melee_Attack();
+			nextMelee = Time.time + meleeRate;
+		}
+	}
+	void Melee_Attack(){
+		Anim.Play (Melee_Clip.name);
+	}
+}
---
+++ Final_project-b815d7093fce15684e33654669acca96c6ae2b5f/Project_4/Assets/Scripts/Stick_attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stick_attack : MonoBehaviour {
+
+	private float 				attack_value = 5f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onTriggerEnter(Collider other){
+		if (other.tag == "Enemy") {
+			other.GetComponent<Enemy>().GetHit(5);
+			//other.gameObject.onhit (attack_value);
+		}
+	}
+}
