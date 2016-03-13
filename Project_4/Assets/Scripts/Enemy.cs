d160496105dbd78369545826bfbf9a8d1bbb0ad2
minttear <minttear@MINTTEARdeMacBook-Pro.local>
2016-03-13 14:51:14 -0400

    melee enemy

---
+++ Project_4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour {
+	//properties
+	public enum enemyType{
+		meleeEne,
+		rangeEne
+	}
+
+	//enemy type
+	protected enemyType _enemyType;
+
+	public enemyType EnemyType{
+		get { return _enemyType;}
+		set { _enemyType = value;}
+	}
+
+	//enemy health 
+	protected int _health;
+
+	public  int Health { 
+		get {return _health;}
+		set { _health = value;
+			if (_health <= 0) {
+				_health = 0;
+				die ();
+			}
+		}
+	}
+
+	//alert range
+	public float alertRange;
+	//attack range public attackRange;
+	public float attackRange;
+	public float speed;
+	//animations
+	public AnimationClip idling,running,attacking,death,getHit;
+
+	[SerializeField]
+	GameObject player;
+	CharacterController controller;
+	protected Animation Anim;
+
+
+	public virtual void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		controller = this.GetComponent < CharacterController> ();
+		Anim =this.GetComponent<Animation> ();
+	}
+		
+
+	public virtual void Update(){
+		if (!Alert () && !InAttackRange ()) {
+			idle ();
+		} else if (!InAttackRange ())
+			chase ();
+		else
+			attack ();
+
+
+
+	}
+
+	//check if it should run to the player
+	bool Alert(){
+		return  (Vector3.Distance(this.transform.position,player.transform.position)<alertRange);
+
+	}
+
+	//check if it is able to attack
+	bool InAttackRange(){
+		return (Vector3.Distance(this.transform.position,player.transform.position)<attackRange);
+
+	}
+
+	//idle
+	public virtual void idle(){
+
+		Anim.Play (idling.name);
+	}
+	//chase the player
+	public virtual void chase(){
+		this.transform.LookAt (player.transform.position);
+		controller.SimpleMove (this.transform.forward*speed);
+		Anim.Play (running.name);
+	}
+
+	public virtual void attack (){
+		this.transform.LookAt (player.transform);
+		Anim.Play (attacking.name);
+	}
+	public virtual void die(){
+		Anim.Play (death.name);
+		if (Anim [death.name].time > 0.95 * Anim [death.name].length)
+			Destroy (this.gameObject);
+	}
+
+	public virtual void GetHit(int damage){
+		Health -= damage;
+
+	}
+	// Use this for initialization
+
+}
---
+++ Project_4/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeEnemy : Enemy {
+	[SerializeField]
+	int attackDamage =20;
+	bool attacked=false;
+	float impactTime =0.1f;
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+		this.Health = 100;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update ();	
+	
+		if( attacked && Anim[attacking.name].time>Anim[attacking.name].length*0.9){
+			
+			attacked=false;
+		}
+	}
+	public override void idle ()
+	{
+		base.idle ();
+	}
+	public override void chase ()
+	{
+		base.chase ();
+	}
+	public override void attack ()
+	{	base.attack ();
+		if( !attacked && Anim[attacking.name].time>this.GetComponent<Animation>()[attacking.name].length*impactTime&&Anim[attacking.name].time<Anim[attacking.name].length*0.9){
+			//do something attack
+			PlayerHealth.Instance.OnHit(attackDamage);
+
+			Debug.Log("attack");
+			attacked=true;
+		}
+
+	}
+}
