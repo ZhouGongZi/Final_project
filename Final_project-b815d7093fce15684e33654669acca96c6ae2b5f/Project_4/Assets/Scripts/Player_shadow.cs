17a1bfa16aa41eb15ac7d35cc73232c246a895ab
YizhouLuo <986732031@qq.com>
2016-03-14 14:12:47 -0400

    asdf

---
+++ Final_project-b815d7093fce15684e33654669acca96c6ae2b5f/Project_4/Assets/Scripts/Player_shadow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_shadow : MonoBehaviour {
+
+	bool viewLocked = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Enemy" && !viewLocked) {
+			transform.LookAt (other.transform);
+			viewLocked = true;
+
+		}
+	}
+
+}
