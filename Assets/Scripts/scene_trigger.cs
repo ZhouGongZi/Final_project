f23515f9c9f10c7edc9adbe2772510f334e6f808
chendifu <chendifu@umich.edu>
2016-03-14 20:32:34 -0400

    demo scene built

---
+++ Assets/Scripts/scene_trigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scene_trigger : MonoBehaviour {
+    public bool triggered = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter (Collision coll)
+    {
+        if (coll.transform.tag == "Player")
+        {
+            triggered = true;
+        }
+    }
+}
